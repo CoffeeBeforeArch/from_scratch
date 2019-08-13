e39d24860e533904e0e5244d2dc8eff976c4b522
Nick <CoffeeBeforeArch@gmail.com>
2019-08-12 21:49:01 -0400

    Add example from the video

--- matrixMul/matrix_mul.cu
+++ matrixMul/matrix_mul.cu
@@ -8,58 +8,55 @@
 using namespace std;
 
 __global__ void matrixMul(int *a, int *b, int *c, int N){
-    // Calculate the global row and column for this thread
+    // Calculate the global row and column for each thread
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
 
-    // Matrix boundary check
+    // Boundary check for our matrix
     if(row < N && col < N){
-        // Accumulate a partial result in tmp
+        // Accumulate a partial result
         int tmp = 0;
         for(int i = 0; i < N; i++){
             tmp += a[row * N + i] * b[i * N + col];
         }
-        
+
         // Write back the result
         c[row * N + col] = tmp;
     }
-
 }
 
-// Initialize a matrix with random numbers between 0 and 10
+// Initializes a square matrix with random numbers between 0-100
 void init_matrix(int *m, int N){
     for(int i = 0; i < N * N; i++){
         m[i] = rand() % 100;
     }
 }
 
-// Verify the solution on the CPU
+// Verify the result on the CPU
 void verify_result(int *a, int *b, int *c, int N){
-    // Temporary variable for partial results
     int tmp;
-
     // For every row...
     for(int i = 0; i < N; i++){
         // For every col...
         for(int j = 0; j < N; j++){
-            // Multiply every element in the row-col pair
+            // For every element in the row-col pair
             tmp = 0;
             for(int k = 0; k < N; k++){
                 tmp += a[i * N + k] * b[k * N + j];
             }
-
-            // Check the result
+            
+            // Check each result
             assert(tmp == c[i * N + j]);
         }
     }
 }
 
 int main(){
-    // Dimensions of our square matrix (2^10 x 2^10 default)
-    int N = 1 << 10;
+    // Set our square matrix dimension (2^10 x 2^10 default) 
+    int N = (1 << 10) + 1;
     size_t bytes = N * N * sizeof(int);
 
-    // Allocate memory
+    // Allocate memory for our matrices
     int *a, *b, *c;
     cudaMallocManaged(&a, bytes);
     cudaMallocManaged(&b, bytes);
@@ -69,21 +66,19 @@ int main(){
     init_matrix(a, N);
     init_matrix(b, N);
 
-    // Set up kernel dimensions
-    // Number of threads in x and y dimensions
-    int THREADS = 16;
-    // Number of blocks in x and y dimensions
-    int BLOCKS = (N + THREADS - 1) / THREADS;
+    // Set our CTA and Grid dimensions
+    int threads = 16;
+    int blocks = (N + threads - 1) / threads;
 
-    // Setup the DIM3 kernel launch arguments
-    dim3 threads(THREADS, THREADS);
-    dim3 blocks(BLOCKS, BLOCKS);
+    // Setup our kernel launch parameters
+    dim3 THREADS(threads, threads);
+    dim3 BLOCKS(blocks, blocks);
 
-    // Launch the kernel
-    matrixMul<<<blocks, threads>>>(a, b, c, N);
+    // Launch our kernel
+    matrixMul<<<BLOCKS, THREADS>>>(a, b, c, N);
     cudaDeviceSynchronize();
 
-    // Call our functional test
+    // Verify the result
     verify_result(a, b, c, N);
 
     cout << "PROGRAM COMPLETED SUCCESSFULLY!" << endl;
